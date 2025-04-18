98081039e0c1f6a275923c92d8c7f85d0f2be984
Ziyad Bhombal <zeebhombal@gmail.com>
2025-04-18 10:28:58 +0530

    tweak: nvim bufferline

--- nvim/.config/nvim/lua/custom/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/custom/plugins/bufferline.lua
@@ -1,38 +1,39 @@
+-- NOTE: Disabled
 local M = {
-  'akinsho/bufferline.nvim',
-  version = '4.7.0',
-  requires = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    local bufferline = require 'bufferline'
-    bufferline.setup {
-      options = {
-        separator_style = 'slope',
-        diagnostics = 'nvim_lsp',
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
-          if context.buffer:current() then
-            return ''
-          end
-          local icon = level:match 'error' and ' ' or ' '
-          return ' ' .. icon .. count
-        end,
-        numbers = function(ordinal, id, lower, raise)
-          return string.format('%d', ordinal)
-        end,
-        offsets = {
-          {
-            filetype = 'neo-tree',
-            text = 'File Explorer',
-            separator = true, -- use a "true" to enable the default, or set your own character
-          },
-        },
-        hover = {
-          enabled = true,
-          delay = 200,
-          reveal = { 'close' },
-        },
-      },
-    }
-  end,
+  -- 'akinsho/bufferline.nvim',
+  -- version = '4.7.0',
+  -- requires = 'nvim-tree/nvim-web-devicons',
+  -- config = function()
+  --   local bufferline = require 'bufferline'
+  --   bufferline.setup {
+  --     options = {
+  --       separator_style = 'slope',
+  --       diagnostics = 'nvim_lsp',
+  --       diagnostics_indicator = function(count, level, diagnostics_dict, context)
+  --         if context.buffer:current() then
+  --           return ''
+  --         end
+  --         local icon = level:match 'error' and ' ' or ' '
+  --         return ' ' .. icon .. count
+  --       end,
+  --       numbers = function(ordinal, id, lower, raise)
+  --         return string.format('%d', ordinal)
+  --       end,
+  --       offsets = {
+  --         {
+  --           filetype = 'neo-tree',
+  --           text = 'File Explorer',
+  --           separator = true, -- use a "true" to enable the default, or set your own character
+  --         },
+  --       },
+  --       hover = {
+  --         enabled = true,
+  --         delay = 200,
+  --         reveal = { 'close' },
+  --       },
+  --     },
+  --   }
+  -- end,
 }
 
 return M
