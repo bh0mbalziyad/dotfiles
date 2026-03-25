6ab9c376c51c94d09f6685ce6f8a7da6d2d87ed7
Ziyad Bhombal <zeebhombal@gmail.com>
2026-03-25 13:26:14 +0530

    chore(neovim): disable nvim-dap

--- nvim/.config/nvim/lua/kickstart/plugins/debug.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/debug.lua
@@ -6,149 +6,153 @@
 -- Primarily focused on configuring the debugger for Go, but can
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
+--
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
-    'mxsdev/nvim-dap-vscode-js',
-  },
-  keys = function(_, keys)
-    local dap = require 'dap'
-    local dapui = require 'dapui'
-    return {
-      -- Basic debugging keymaps, feel free to change to your liking!
-      { '<leader>dt', dap.toggle_breakpoint, desc = '[D]ebug: Breakpoint [T]oggle' },
-      { '<leader>dc', dap.continue, desc = '[D]ebug: [C]ontinue' },
-      { '<leader>dC', dap.disconnect, desc = '[D]ebug: Dis[c]onnect' },
-      { '<leader>do', dap.continue, desc = '[D]ebug: Step [o]ver' },
-      { '<leader>dO', dap.continue, desc = '[D]ebug: Step [O]ut' },
-      { '<leader>dI', dap.continue, desc = '[D]ebug: Step [I]nto' },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      { '<leader>dD', dapui.toggle, desc = '[D]ebug: See last session result.' },
-      unpack(keys),
-    }
-  end,
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
-    local mason_registry = require 'mason-registry'
-
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
-        'js-debug-adapter',
-      },
-    }
-
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
-
-    vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
-    vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
-
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    local vscode_js_debug_dap = mason_registry.get_package 'js-debug-adapter'
-
-    if not dap.adapters['pwa-node'] then
-      require('dap').adapters['pwa-node'] = {
-        type = 'server',
-        host = 'localhost',
-        port = '${port}',
-        executable = {
-          command = 'node',
-          -- 💀 Make sure to update this path to point to your installation
-          args = {
-            vscode_js_debug_dap:get_install_path() .. '/js-debug/src/dapDebugServer.js',
-            '${port}',
-          },
-        },
-      }
-    end
-    if not dap.adapters['node'] then
-      dap.adapters['node'] = function(cb, config)
-        if config.type == 'node' then
-          config.type = 'pwa-node'
-        end
-        local nativeAdapter = dap.adapters['pwa-node']
-        if type(nativeAdapter) == 'function' then
-          nativeAdapter(cb, config)
-        else
-          cb(nativeAdapter)
-        end
-      end
-    end
-
-    local js_filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' }
-
-    local vscode = require 'dap.ext.vscode'
-    vscode.type_to_filetypes['node'] = js_filetypes
-    vscode.type_to_filetypes['pwa-node'] = js_filetypes
-
-    for _, language in ipairs(js_filetypes) do
-      if not dap.configurations[language] then
-        dap.configurations[language] = {
-          {
-            type = 'pwa-node',
-            request = 'launch',
-            name = 'Launch file',
-            program = '${file}',
-            cwd = '${workspaceFolder}',
-          },
-          {
-            type = 'pwa-node',
-            request = 'attach',
-            name = 'Attach',
-            processId = require('dap.utils').pick_process,
-            cwd = '${workspaceFolder}',
-          },
-        }
-      end
-    end
-  end,
 }
+
+-- return {
+--   -- NOTE: Yes, you can install new plugins here!
+--   'mfussenegger/nvim-dap',
+--   -- NOTE: And you can specify dependencies as well
+--   dependencies = {
+--     -- Creates a beautiful debugger UI
+--     'rcarriga/nvim-dap-ui',
+--
+--     -- Required dependency for nvim-dap-ui
+--     'nvim-neotest/nvim-nio',
+--
+--     -- Installs the debug adapters for you
+--     'williamboman/mason.nvim',
+--     'jay-babu/mason-nvim-dap.nvim',
+--
+--     -- Add your own debuggers here
+--     'mxsdev/nvim-dap-vscode-js',
+--   },
+--   keys = function(_, keys)
+--     local dap = require 'dap'
+--     local dapui = require 'dapui'
+--     return {
+--       -- Basic debugging keymaps, feel free to change to your liking!
+--       { '<leader>dt', dap.toggle_breakpoint, desc = '[D]ebug: Breakpoint [T]oggle' },
+--       { '<leader>dc', dap.continue, desc = '[D]ebug: [C]ontinue' },
+--       { '<leader>dC', dap.disconnect, desc = '[D]ebug: Dis[c]onnect' },
+--       { '<leader>do', dap.continue, desc = '[D]ebug: Step [o]ver' },
+--       { '<leader>dO', dap.continue, desc = '[D]ebug: Step [O]ut' },
+--       { '<leader>dI', dap.continue, desc = '[D]ebug: Step [I]nto' },
+--       -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+--       { '<leader>dD', dapui.toggle, desc = '[D]ebug: See last session result.' },
+--       unpack(keys),
+--     }
+--   end,
+--   config = function()
+--     local dap = require 'dap'
+--     local dapui = require 'dapui'
+--     local mason_registry = require 'mason-registry'
+--
+--     require('mason-nvim-dap').setup {
+--       -- Makes a best effort to setup the various debuggers with
+--       -- reasonable debug configurations
+--       automatic_installation = true,
+--
+--       -- You can provide additional configuration to the handlers,
+--       -- see mason-nvim-dap README for more information
+--       handlers = {},
+--
+--       -- You'll need to check that you have the required things installed
+--       -- online, please don't ask me how to install them :)
+--       ensure_installed = {
+--         -- Update this to ensure that you have the debuggers for the langs you want
+--         'delve',
+--         'js-debug-adapter',
+--       },
+--     }
+--
+--     -- Dap UI setup
+--     -- For more information, see |:help nvim-dap-ui|
+--     dapui.setup {
+--       -- Set icons to characters that are more likely to work in every terminal.
+--       --    Feel free to remove or use ones that you like more! :)
+--       --    Don't feel like these are good choices.
+--       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+--       controls = {
+--         icons = {
+--           pause = '⏸',
+--           play = '▶',
+--           step_into = '⏎',
+--           step_over = '⏭',
+--           step_out = '⏮',
+--           step_back = 'b',
+--           run_last = '▶▶',
+--           terminate = '⏹',
+--           disconnect = '⏏',
+--         },
+--       },
+--     }
+--
+--     vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+--     vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+--
+--     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+--     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+--     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+--
+--     local vscode_js_debug_dap = mason_registry.get_package 'js-debug-adapter'
+--
+--     if not dap.adapters['pwa-node'] then
+--       require('dap').adapters['pwa-node'] = {
+--         type = 'server',
+--         host = 'localhost',
+--         port = '${port}',
+--         executable = {
+--           command = 'node',
+--           -- 💀 Make sure to update this path to point to your installation
+--           args = {
+--             vscode_js_debug_dap:get_install_path() .. '/js-debug/src/dapDebugServer.js',
+--             '${port}',
+--           },
+--         },
+--       }
+--     end
+--     if not dap.adapters['node'] then
+--       dap.adapters['node'] = function(cb, config)
+--         if config.type == 'node' then
+--           config.type = 'pwa-node'
+--         end
+--         local nativeAdapter = dap.adapters['pwa-node']
+--         if type(nativeAdapter) == 'function' then
+--           nativeAdapter(cb, config)
+--         else
+--           cb(nativeAdapter)
+--         end
+--       end
+--     end
+--
+--     local js_filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' }
+--
+--     local vscode = require 'dap.ext.vscode'
+--     vscode.type_to_filetypes['node'] = js_filetypes
+--     vscode.type_to_filetypes['pwa-node'] = js_filetypes
+--
+--     for _, language in ipairs(js_filetypes) do
+--       if not dap.configurations[language] then
+--         dap.configurations[language] = {
+--           {
+--             type = 'pwa-node',
+--             request = 'launch',
+--             name = 'Launch file',
+--             program = '${file}',
+--             cwd = '${workspaceFolder}',
+--           },
+--           {
+--             type = 'pwa-node',
+--             request = 'attach',
+--             name = 'Attach',
+--             processId = require('dap.utils').pick_process,
+--             cwd = '${workspaceFolder}',
+--           },
+--         }
+--       end
+--     end
+--   end,
+-- }
