eb10bd0ecfe94d891b41b5839c91811ce3576ef0
Ziyad Bhombal <zeebhombal@gmail.com>
2026-04-10 15:25:45 +0530

    tweak(nvim): colorscheme

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -12,13 +12,24 @@
 --   end,
 -- }
 
+-- local M = {
+--   'rebelot/kanagawa.nvim',
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require('kanagawa').setup()
+--     vim.cmd 'colorscheme kanagawa'
+--   end,
+-- }
+
 local M = {
-  'rebelot/kanagawa.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
-    require('kanagawa').setup()
-    vim.cmd 'colorscheme kanagawa'
+    require('tokyonight').setup()
+    vim.cmd 'colorscheme tokyonight-night'
   end,
 }
 
