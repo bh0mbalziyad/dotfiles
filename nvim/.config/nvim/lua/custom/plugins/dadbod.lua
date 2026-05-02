71182da2b4a76f5039ee8bfb9d52842a3f0137d0
Ziyad Bhombal <zeebhombal@gmail.com>
2026-05-02 09:00:16 +0530

    chore(nvim): revert config

--- nvim/.config/nvim/lua/custom/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/custom/plugins/dadbod.lua
@@ -2,6 +2,16 @@ local M = {
   'tpope/vim-dadbod',
   'kristijanhusak/vim-dadbod-completion',
   'kristijanhusak/vim-dadbod-ui',
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      'tpope/vim-dadbod',
+      'kristijanhusak/vim-dadbod-completion',
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>dbu', '<cmd>DBUIToggle<CR>', { desc = '[DBUI] [T]oggle' })
+    end,
+  },
 }
 
 return M
